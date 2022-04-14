de86f95365c310caa00b16ce2de0fe58657d04de
Yanushevich <maksim_yanushevich@mail.ru>
2022-04-14 14:57:15 +0500

    Signed-off-by: Yanushevich <maksim_yanushevich@mail.ru>

--- index.php
+++ index.php
@@ -1,1 +1,21 @@
-<?php include_once(“home.html”); ?>
+<title> Янушевич Максим ПИ-322</title>
+
+
+<table border="0" cellspacing="10">
+
+   <tr>
+    <th align="left">Лабораторная работа №1:</th>
+   </tr>
+
+<tr>
+<td>
+
+<ul>
+  <li><a href="lab1/Lab1.html">Задание 1</a></li>
+
+</ul>
+
+
+</tr>
+
+</table>
