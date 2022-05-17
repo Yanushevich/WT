2733c96b9fdac775ebae717a8ddb50251f065b6f
Yanushevich <maksim_yanushevich@mail.ru>
2022-05-17 20:28:43 +0500

    Вторая лабораторная, первые 4 задания и необходимые файлы

--- index.php
+++ index.php
@@ -4,7 +4,8 @@
 <table border="0" cellspacing="10">
 
    <tr>
-    <th align="left">Лабораторная работа №1:</th>
+     <th align="left">Лабораторная работа №1:</th>
+     <th align="left">Лабораторная работа №2:</th>
    </tr>
 
 <tr>
@@ -19,8 +20,21 @@
   <li><a href="lab1/Lab6.html">Задание 6</a></li>
   <li><a href="lab1/Lab7.html">Задание 7</a></li>
 </ul>
+</td>
 
+<td>
+<ul>
+  <li><a href="lab2/Lab1.html">Задание 1</a></li>
+  <li><a href="lab2/Lab2.html">Задание 2</a></li>
+  <li><a href="lab2/Lab3.html">Задание 3</a></li>
+  <li><a href="lab2/Lab4.html">Задание 4</a></li>
+  <li><a href="lab2/Lab5.html">Задание 5</a></li>
+  <li><a href="lab2/Lab6.html">Задание 6</a></li>
+  <li><a href="lab2/Lab7.html">Задание 7</a></li>
+</ul>
+</td>
 
 </tr>
+
 
 </table>
